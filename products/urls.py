a608e40e628494cce9bc65d1400e35a9e600fd8c
Luiz Felipe Rodrigues Dantas <seu.email@provedor.com>
2025-10-03 01:13:22 -0300

    Adiciona view para criar superuser programaticamente

--- products/urls.py
+++ products/urls.py
@@ -6,4 +6,17 @@ from .views import ProductListCreateAPIView, ProductRetrieveUpdateDestroyAPIView
 urlpatterns = [
     path('', ProductListCreateAPIView.as_view(), name='product-list-create'),
     path('<int:pk>/', ProductRetrieveUpdateDestroyAPIView.as_view(), name='product-detail'),
+
+]# products/urls.py (adicionar a nova path)
+
+from django.urls import path
+from .views import ProductListCreateAPIView, ProductRetrieveUpdateDestroyAPIView, create_superuser_view # Adicione create_superuser_view
+
+urlpatterns = [
+    # ... suas urls existentes ...
+    path('', ProductListCreateAPIView.as_view(), name='product-list-create'),
+    path('<int:pk>/', ProductRetrieveUpdateDestroyAPIView.as_view(), name='product-detail'),
+
+    # Adicione a linha abaixo
+    path('create-super-user-temp-12345/', create_superuser_view, name='create-superuser'),
 ]
--- products/views.py
+++ products/views.py
@@ -11,3 +11,19 @@ class ProductListCreateAPIView(generics.ListCreateAPIView):
 class ProductRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    # products/views.py (adicionar no final)
+
+from django.http import HttpResponse
+from django.contrib.auth import get_user_model
+import os
+
+def create_superuser_view(request):
+    User = get_user_model()
+    username = os.environ.get('ADMIN_USER')
+    password = os.environ.get('ADMIN_PASSWORD')
+
+    if not User.objects.filter(username=username).exists():
+        User.objects.create_superuser(username=username, password=password)
+        return HttpResponse("Superusuário criado com sucesso!")
+    else:
+        return HttpResponse("Superusuário já existe.")
